346cc20d60171125d5184b079dc2f91b346825e8
Andrew Makousky <mako0042@umn.edu>
2012-12-31 13:58:44 -0600

    Save file.

---
+++ src/glib/glib.h
@@ -0,0 +1,16 @@
+/* A very limited wrapper and replacement for GLib.
+
+   Only use this code in place of the real GLib if code base size is
+   of concern.  */
+
+#ifndef GLIB_H
+#define GLIB_H
+
+#include <glib/garray.h>
+#include <glib/glist.h>
+#include <glib/gmacros.h>
+#include <glib/gslice.h>
+#include <glib/gstring.h>
+#include <glib/gtypes.h>
+
+#endif /* not GLIB_H */
